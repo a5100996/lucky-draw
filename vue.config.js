5e67912182fa61736e887fe7b06f742761f44208
a5100996 <a5100996@gmail.com>
2024-02-27 00:33:30 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  devServer: {
-    port: 80 // 更改端口为 80
-  },
-  publicPath: process.env.NODE_ENV === 'production' ? '/lucky-draw' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
 };
